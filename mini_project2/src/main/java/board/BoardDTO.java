b457ec7364f01521d0021ef9a8920d24d3c76b59
ysseol <virtus1313@gmail.com>
2025-06-02 17:46:32 +0900

    ...

--- mini_project2/src/main/java/board/BoardDTO.java
+++ mini_project2/src/main/java/board/BoardDTO.java
@@ -1,14 +1,10 @@
 package board;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 
 @Getter
 @Setter
-@NoArgsConstructor
 public class BoardDTO {
 	private int num;
 	private String id;
--- mini_project2/src/main/java/member/MemberDTO.java
+++ mini_project2/src/main/java/member/MemberDTO.java
@@ -1,14 +1,10 @@
 package member;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 
 @Getter
 @Setter
-@NoArgsConstructor
 public class MemberDTO {
 	private String userid;
 	private String pwd;
